f89f4a0cc23dc9d46fe8161e0b346cfd75c2a793
danthe <danthe96@gmx.de>
2013-01-13 18:22:46 +0100

    Added Texture/Material Selection
    Purely graphical at the moment, but textures can be selected. This is
    only the interface, we might as well implement material selection.

--- workspace/ProjectOuhNew/src/windowManager/Main.java
+++ workspace/ProjectOuhNew/src/windowManager/Main.java
@@ -2,7 +2,17 @@ package windowManager;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -20,77 +30,108 @@ import com.jme3.system.JmeCanvasContext;
 
 import editor.SwingCanvasTest;
 
-public class Main implements Runnable {
+public class Main {
 	
 	static SwingCanvasTest canvasApplication;
+
 	
 	public static void main(String[] args) {
 
-		java.awt.EventQueue.invokeLater(new Main());
-	}
+		java.awt.EventQueue.invokeLater(new Runnable() {
+		      public void run() {
+		    	  AppSettings settings = new AppSettings(true);
+		    	  settings.setResolution(640, 480);
+		    	  settings.setBitsPerPixel(32);
+		    	  settings.setRenderer("LWJGL-OpenGL2");
+		    	  settings.setSamples(4);
+		    	  settings.setFrameRate(120);
+		    	  
+		    	  
+		    	  
+		    	  canvasApplication = new SwingCanvasTest();
+		    	  canvasApplication.setSettings(settings);
+		    	  canvasApplication.createCanvas(); // create canvas!
+		    	  JmeCanvasContext ctx = (JmeCanvasContext) canvasApplication.getContext();
+		    	  ctx.setSystemListener(canvasApplication);
+		    	  Dimension dim = new Dimension(640, 480);
+		    	  ctx.getCanvas().setPreferredSize(dim);
+		    	  
+		    	  
+		    	  JFrame window = new JFrame("Swing Application");
+					window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					
+					// add all your Swing components ...		    	  
+					JPanel panel = new JPanel(new FlowLayout()); // a panel
+					
+					JMenuBar menuBar = new JMenuBar();
+					JMenu fileMenu = new JMenu( "Datei", true);
+					menuBar.add( fileMenu );
+					
+					fileMenu.add( new NewLevelAction(canvasApplication,"Neu") );
+					fileMenu.add( new LoadLevelAction("Öffnen") );
+					fileMenu.add( new SaveLevelAction("Speichern") );
+					fileMenu.add( new CloseEditorAction("Beenden") );
+					
+					JMenu helpMenu = new JMenu( "Hilfe" );
+					menuBar.add( helpMenu );
+					helpMenu.add( new JMenuItem("Über das Programm") );
+					
+					window.setJMenuBar( menuBar );
+					
+					class MatPanel extends JPanel{
+						public void initButtons(int j, String[] path){
+							BufferedImage[] buttonIcon = new BufferedImage[j];
+							JButton[] matButton = new JButton[j];
+							
+							for(int i=0;i<j;i++)
+							try {
+								buttonIcon[i] = ImageIO.read(ClassLoader.getSystemClassLoader().getResourceAsStream(path[i]));  
+								matButton[i] = new JButton(new ImageIcon(buttonIcon[i].getScaledInstance(50, 50, 0)));
+								final String imgpath = path[i];
+								matButton[i].addActionListener( new ActionListener() {
+									  @Override public void actionPerformed( ActionEvent e ) {
+									      
+									  }
+									} );
+								this.add(matButton[i]);
+								System.out.println("dx");
+							} catch (IOException e) {System.out.println("acfv");}
+					}
+					}
+					
+					MatPanel p = new MatPanel();
+					
+					Dimension d = new Dimension(240,480);
+					p.setSize(d);
+					p.setPreferredSize(d);
+					p.setMinimumSize(d);
+						
+					JTabbedPane tabbedPane = new JTabbedPane();
+					tabbedPane.addTab("Material", p);
+					tabbedPane.addTab("Model", new JPanel());
+					tabbedPane.addTab("Platzhalter", new JPanel());
+					
+					String[] paths = {"Assets/test.png","Assets/Wandtest.png"};
+					
+					p.initButtons(paths.length, paths);
+					
+					panel.add(tabbedPane);
+					
+					// add the JME canvas
+					panel.add(ctx.getCanvas());
+					
+					window.add(panel);
+					window.pack();
+					window.setVisible(true);
+		    	  
+		    	  canvasApplication.startCanvas();
 
-	@Override
-	public void run() {
-  	  AppSettings settings = new AppSettings(true);
-  	  settings.setBitsPerPixel(32);
-  	  settings.setRenderer("LWJGL-OpenGL2");
-  	  settings.setSamples(4);
-  	  settings.setFrameRate(120);
-  	    	    	  
-  	  canvasApplication = new SwingCanvasTest();
-  	  canvasApplication.setSettings(settings);
-  	  canvasApplication.createCanvas(); // create canvas!
-  	  JmeCanvasContext ctx = (JmeCanvasContext) canvasApplication.getContext();
-  	  ctx.setSystemListener(canvasApplication);
-  	  Dimension dim = new Dimension(800, 600);
-  	  ctx.getCanvas().setPreferredSize(dim);
-  	  
-  	  
-  	  JFrame window = new JFrame("Swing Application");
-			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
-			// add all your Swing components ...		    	  
-			JPanel panel = new JPanel(new FlowLayout()); // a panel
-			
-			JMenuBar menuBar = new JMenuBar();
-			JMenu fileMenu = new JMenu( "Datei", true);
-			menuBar.add( fileMenu );
-			
-			fileMenu.add( new NewLevelAction(canvasApplication,"Neu") );
-			fileMenu.add( new LoadLevelAction("Öffnen") );
-			fileMenu.add( new SaveLevelAction("Speichern") );
-			fileMenu.add( new CloseEditorAction("Beenden") );
-			
-			JMenu helpMenu = new JMenu( "Hilfe" );
-			menuBar.add( helpMenu );
-			helpMenu.add( new JMenuItem("Über das Programm") );
-			
-			window.setJMenuBar( menuBar );
-			
-			JPanel p=new JPanel();
-			Dimension d =new Dimension(240,480);
-			p.setSize(d);
-			p.setPreferredSize(d);
-			p.setMinimumSize(d);
-				
-			JTabbedPane tabbedPane = new JTabbedPane();
-			tabbedPane.addTab("Material", p);
-			tabbedPane.addTab("Model", new JPanel());
-			tabbedPane.addTab("Platzhalter", new JPanel());
-			
-			panel.add(tabbedPane);
-			
-			// add the JME canvas
-			panel.add(ctx.getCanvas());
-			
-			window.add(panel);
-			window.pack();
-			window.setVisible(true);
-  	  
-  	  canvasApplication.startCanvas();
-
-    
+		      }
+		    });
 		
+		
+		
+
 	}
 
 }
